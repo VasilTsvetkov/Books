35cbcad24e8e318cdd2f813368c043c984894e1a
Vasil Tsvetkov <vasiltsvetkov20@gmail.com>
2025-04-05 16:17:04 +0300

    Added UpsertBookRequest model

---
+++ Books.Contracts/Requests/UpsertBookRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Books.Contracts.Requests
+{
+	public class UpsertBookRequest
+	{
+        public required string Title { get; init; }
+
+        public required string Author { get; init; }
+
+        public required int YearOfRelease { get; init; }
+
+        public required string Genre { get; init; }
+    }
+}
