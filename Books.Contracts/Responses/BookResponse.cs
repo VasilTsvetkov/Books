fd75d0189e26cc624d4af0de7bb368eb7e44d248
Vasil Tsvetkov <vasiltsvetkov20@gmail.com>
2025-04-05 16:24:50 +0300

    Added response models

---
+++ Books.Contracts/Responses/BookResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Books.Contracts.Responses
+{
+	public class BookResponse
+	{
+		public required Guid Id { get; init; }
+
+		public required string Title { get; init; }
+
+		public required string Author { get; init; }
+
+		public required int YearOfRelease { get; init; }
+
+		public required string Genre { get; init; }
+	}
+}
---
+++ Books.Contracts/Responses/BooksResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Books.Contracts.Responses
+{
+	public class BooksResponse
+	{
+		public required IEnumerable<BookResponse> Books { get; init; } = Enumerable.Empty<BookResponse>();
+    }
+}
